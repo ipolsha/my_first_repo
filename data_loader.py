26addafbd33817f18b7f865cfcf5610efa5a95b2
ipolsha <ipolsha239@gmail.com>
2025-09-28 17:11:08 +0300

    homework_3

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,29 @@
 import pandas as pd
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/1scmkeENxadknow2rZ6H9LiG9m_BJmkBH/export?format=csv')
+df.head(10)
+df1 = pd.read_csv('https://docs.google.com/spreadsheets/d/1G6m-QoLgdWbOV3rSUBOaxn1cDQKkKk1H/export?format=csv').rename(columns={'SMDBid': 'SMDB_id'})
+df1.head(10)
 
-raw_data = pd.read_excel(
-    "C:\MyPythonProjects\my_first_repo\data\siRNAmod_db_parsed_concs.xlsx"
-)
-print(raw_data.head(10))
+data = pd.merge(df, df1, on='SMDB_id')
+data = data.drop(columns=['Melting point (°C)', 'Reference Link', 'Field1_links', 'Trust', 'Efficacy_y', 'Field8_links', 'Reference'])
+
+print(data.info())
+
+string = ['Experiment used to check activity', 'Target gene', 'Cell or Organism used', 'Transfection method', 'siRNA sense', 'siRNA antisense', 'Modification sense', 'Modification antisense', 'Position sense', 'Position antisense']
+data = data.dropna(subset=string)
+data = data.convert_dtypes()
+print(data.info())
+
+data['Concentration new'] = data['Concentration'].str.split(' ').str[0].str.extract(r'^(\d+\.?\d*)$', expand=False)
+data['id_'] = data['SMDB_id'].str.split('SM').str[1].str.extract(r'^(\d+\.?\d*)$', expand=False)
+data['Duration after transfection new'] = data['Duration after transfection'].str.split(' ').str[0]
+
+data = data.dropna(subset=['Concentration new', 'Duration after transfection new'])
+data['Concentration new'] = data['Concentration new'].astype(float)
+data['id_'] = data['id_'].astype(int)
+data['Duration after transfection new'] = data['Duration after transfection new'].astype(int)
+data = data.drop(columns=['SMDB_id', 'Concentration', 'Duration after transfection'])
+print('Типы данных после преобразования:')
+print(data.dtypes)
+data.to_csv('new_data.csv')
+print('Датасет сохранен под именем new_data.csv')
